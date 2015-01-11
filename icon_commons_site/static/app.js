cecb0213b9b6c2a22344f35b74b5dc838cddeb2c
Ian Schneider <planablediglet@gmail.com>
2015-01-10 17:28:49 -0700

    icon editor + coloring w/ more info
    - codemirror for editing
    - coloring UI + link generation
    - list collection + tags w/ link
    
    see #2 and #3

--- icon_commons_site/static/app.js
+++ icon_commons_site/static/app.js
@@ -6,11 +6,11 @@
             .when('/', {
                 templateUrl: '/static/home.html'
             })
-            .when('/tags', {
+            .when('/tags/:tag?', {
                 controller: 'Tags',
                 templateUrl: '/static/tags.html'
             })
-            .when('/icons/collections/:collection', {
+            .when('/icons/collections/:collection*', {
                 controller: 'Collection',
                 templateUrl: '/static/collection.html'
             })
@@ -49,15 +49,69 @@
         };
     });
 
-    module.controller('IconView', function($scope, $routeParams, $http, $sce, SVG) {
-        $scope.data = {};
-        $http.get('/icons/icon/' + $routeParams.icon).then(function(response) {
-            $scope.data.uri = SVG.getDataURI(response.data);
-            $scope.data.rawsvg = response.data;
+    function process(svg, fill, stroke) {
+        var element = angular.element(document.createElement('div'));
+        element.html(svg);
+        // @todo make smarter
+        ['path', 'rect'].forEach(function(el) {
+            angular.forEach(element.find(el), function(e) {
+                // @todo does it make sense to override stroke width?
+                e = angular.element(e);
+                var css = {
+                    opacity: 1
+                };
+                var existingFill = e.css('fill') || e.attr('fill') || '';
+                console.log(existingFill);
+                if (existingFill != 'none' && existingFill != 'rgb(255, 255, 255)' && existingFill.toLowerCase() != '#ffffff') {
+                    css.fill = fill;
+                }
+                var existingStroke = e.css('stroke') || e.attr('stroke');
+                if (existingStroke != 'none') {
+                    css.stroke = stroke;
+                }
+                e.css(css);
+            });
         });
+        return element.html();
+    }
+
+    module.controller('IconView', function($scope, $location, $http, SVG) {
+        var path = $location.path();
+        function colorIcon() {
+            if ($scope.data) {
+                $scope.colored = {
+                    uri: SVG.getDataURI(process($scope.data.rawsvg, $scope.color.fill, $scope.color.stroke))
+                };
+                var port = '';
+                if ($location.port() != 80) {
+                    port = ':' + $location.port();
+                }
+                $scope.coloredLink = $location.protocol() +"://" + $location.host() + port + path + '?' + 'fill=' + encodeURIComponent($scope.color.fill) + "&stroke=" + encodeURIComponent($scope.color.stroke);
+            }
+        }
+        $scope.color = {
+            fill:'#ff0000',
+            stroke:'#0000ff'
+        };
+        $http.get(path + '/info').then(function(response) {
+            $scope.iconInfo = response.data;
+        });
+        $http.get(path).then(function(response) {
+            $scope.data = {
+                uri: SVG.getDataURI(response.data),
+                rawsvg: response.data
+            };
+            colorIcon();
+        });
+        $scope.change = function(svg) {
+            $scope.data.uri = SVG.getDataURI(svg);
+            $scope.rawsvg = svg;
+            colorIcon();
+        };
         $scope.sce = function() {
             $scope.data.uri = SVG.getDataURI($scope.data.rawsvg);
         };
+        $scope.$watch('color', colorIcon, true);
     });
 
     module.controller('Collection', function($scope, $routeParams, $http, IconService) {
@@ -76,8 +130,9 @@
         });
     });
 
-    module.controller('Tags', function($scope, $http, IconService) {
+    module.controller('Tags', function($scope, $http, $routeParams, IconService) {
         $scope.search = {};
+        $scope.searchPhrase = $routeParams.tag;
         $scope.getTags = function(val) {
             var promise = $http.get('/icons/search/tags', {
                 params: {
@@ -99,6 +154,9 @@
                 IconService.handleCollections($scope.search, response);
             });
         };
+        if ($routeParams.tag) {
+            $scope.typeaheadSelect($routeParams.tag);
+        }
     });
 
     module.controller('App', function($scope, $http, IconService) {
@@ -110,6 +168,48 @@
             }).then(function(response) {
                 IconService.handleCollections(collection, response);
             });
+        };
+    });
+
+    module.directive('svgImage', function() {
+        return {
+            restrict: 'E',
+            template: '<img ng-src="{{src.uri}}"></img>',
+            scope: {
+                'src': '='
+            },
+            link: function(scope, element, attrs) {
+                var img = element.find('img');
+                img.on('error', function() {
+                    console.log('error', arguments);
+                });
+                img.on('load', function() {
+                    console.log('load', arguments);
+                });
+            }
+        };
+    });
+
+    module.directive('svgEditor', function() {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                'data': '=',
+                'change': '='
+            },
+            link: function(scope, element, attrs) {
+                var cm = CodeMirror(element[0], {
+                    value: scope.data.rawsvg,
+                    mode: "xml"
+                });
+                cm.setSize('100%','50%');
+                cm.on('change', function() {
+                    scope.$apply(function() {
+                        scope.change(cm.getValue());
+                    });
+                });
+            }
         };
     });
 
